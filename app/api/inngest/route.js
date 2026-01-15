f29f4ff12eb6383bd0da31b4a5f382e1888d027a
mjgar <mjgar@example.com>
2026-01-15 18:55:19 +0800

    update

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { syncUserCreation, syncUserUpdation } from "@/inngest/function";
+import { syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/inngest/function";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
